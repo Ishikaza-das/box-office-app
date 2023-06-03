92715c025c883d22cd36bb0f01812ddd58119954
Ritesh <riteshdas8652@gmail.com>
2023-06-03 19:32:02 +0530

    Box-office

---
+++ Projects/box-office/src/pages/Home.jsx
@@ -0,0 +1,11 @@
+import { Link } from 'react-router-dom';
+
+const Home = () => {
+  return (
+    <div>
+      <link to="/Starred">Go to starred page</link>
+    </div>
+  );
+};
+
+export default Home;
